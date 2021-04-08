8f4bacbba243ec822720e349223209e3f4d03547
Nhân Nguyễn <nhnhan.18it1@sict.udn.vn>
2021-04-08 09:37:18 +0700

    first

--- index.js
+++ index.js
@@ -5,18 +5,31 @@ var https = require("http").createServer(app).listen(process.env.PORT || 3000)
 var io = require("socket.io").listen(https);
 
 var car=[]
+var remote=[]
 
 app.get("/",(req, res)=>{
     res.send("hello")
 })
 
 io.on('connection',(socket)=>{
-    console.log(socket.id);
+    console.log("connected: "+socket.id);
+    remote.push(socket)
     socket.on('send_position',(data)=>{
-        socket.broadcast.emit('sv_send_position',data)
+        remote.forEach(element => {
+            element.emit("sv_send_position",data)
+        });
     })
     socket.on('regis',(data)=>{
-        socket.car_infor = data
-        car.push(socket)
+        if(data=="remote"){
+            remote.push(socket)
+        }
+        else{
+            socket.car_infor = data
+            car.push(socket)
+        }
+        
+    })
+    socket.on("disconnect",()=>{
+        remote.remote(socket)
     })
 })
